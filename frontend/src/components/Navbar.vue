f513e92ab86facfbedc11074a965e0557073e5c9
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2025-03-06 08:08:37 +1100

    feat: bootstrap frontend (#1)

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Menubar from 'primevue/menubar';
+import { ref } from 'vue';
+
+const items = ref([
+  {
+    label: 'Home',
+    icon: 'i-mdi-home',
+  },
+]);
+</script>
+
+<template>
+  <Menubar :model="items" />
+</template>
---
+++ frontend/uno.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, presetIcons, presetMini } from 'unocss'
+
+export default defineConfig({
+  shortcuts: {
+    'heading': 'text-4xl font-bold',
+  },
+  presets: [
+    presetIcons({
+      collections: {
+        mdi: () => import('@iconify-json/mdi/icons.json').then(i => i.default),
+      }
+    }),
+    presetMini()
+  ]
+})
