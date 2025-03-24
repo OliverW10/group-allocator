4223bfb90bd0cbbb248865ccb07f38c08bbc5ea5
Oliver W <34265013+OliverW10@users.noreply.github.com>
2025-03-24 23:01:53 +1100

    feat: Add solver scaffolding (#18)

--- backend/GroupAllocator/Database/Model/ProjectModel.cs
+++ backend/GroupAllocator/Database/Model/ProjectModel.cs
@@ -2,7 +2,7 @@
 
 public class ProjectModel
 {
-	public required int Id { get; set; }
+	public int Id { get; set; }
 	public required string Name { get; set; }
 	public required bool RequiresContract { get; set; }
 	public required ClientModel Client { get; set; }
