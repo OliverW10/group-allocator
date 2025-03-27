b06ecd5ed6e112a4a76a750f6e9318a9f0ca78c6
Olivia Stewart <liv.stewart2004@gmail.com>
2025-03-28 10:20:20 +1100

    a

---
+++ frontend/src/models/ProjectDto.ts
@@ -0,0 +1,6 @@
+export default interface ProjectDto {
+    id: number
+    name: string
+    description: string
+    maxGroupSize: number
+}
---
+++ frontend/src/views/Projects.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1 class="heading">Projects</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type ProjectDto from '../models/ProjectDto';
+import { ref } from 'vue';
+
+
+const projectData = ref(
+    [
+    {
+        id: 1,
+        name: "Project 1",
+        description: "Description 1",
+    }] as ProjectDto[]);
+</script>
