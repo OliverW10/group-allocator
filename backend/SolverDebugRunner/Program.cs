acaeb00acd683491e2dde63c9f2896e38ea2ce10
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2025-04-02 20:01:21 +1100

    fix build error

--- backend/SolverDebugRunner/Program.cs
+++ backend/SolverDebugRunner/Program.cs
@@ -41,11 +41,15 @@ List<ProjectModel> projects = [
         Client = clients[0],
         Name = "AI Slop",
         RequiresContract = false,
+        MinStudents = 1,
+        MaxStudents = 2
     },
     new ProjectModel() {
         Client = clients[0],
         Name = "Crypto Scam",
         RequiresContract = true,
+        MinStudents = 3,
+        MaxStudents = 4
     }
 ];
 
