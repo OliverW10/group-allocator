0d437db183268cb4cb5e472768143efbfc78f644
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2025-04-02 19:36:53 +1100

    make form betterer without copypasta this time

--- frontend/src/models/GroupAllocationOption.ts
+++
@@ -1,4 +0,0 @@
-export default interface GroupAllocationOption {
-    id: number
-    name: string
-}
--- frontend/src/views/StudentPreferencesView.vue
+++ frontend/src/views/StudentPreferencesView.vue
@@ -1,28 +1,15 @@
 <template>
-    <div>
-        <h1 class="heading">Student Preference Form</h1>
-        <Card>
+    <div class="flex flex-col gap-4 p-4 h-screen justify-center">
+        <h1 class="heading text-center mb-4">Submit Preferences</h1>
+        <Card class="min-w-3xl mx-auto">
             <template #content>
                 <form class="flex flex-col gap-4" @submit.prevent="submitForm">
                     <div class="flex-auto">
-                        <label for="firstName" class="font-bold block mb-2">First Name</label>
-                        <InputText id="firstName" v-model="form.firstName" placeholder="Enter First Name" />
-                    </div>
-                    <div class="flex-auto">
-                        <label for="lastName" class="font-bold block mb-2">Last Name</label>
-                        <InputText id="lastName" v-model="form.lastName" placeholder="Enter Last Name" />
-                    </div>
-                    <div class="flex-auto">
-                        <label for="studentNumber" class="font-bold block mb-2">Student Number</label>
-                        <InputText id="studentNumber" v-model="form.studentNumber" placeholder="Enter Student Number" />
-                    </div>
-                    <div class="flex-auto">
                         <label for="studentEmail" class="font-bold block mb-2">Student Email</label>
-                        <InputText id="studentEmail" v-model="form.email"
-                            placeholder="example.email@student.uts.edu.au" />
+                        <InputText id="studentEmail" :placeholder="authStore.userInfo?.email" disabled />
                     </div>
-                    <div class="flex-auto">
-                        <Checkbox id="consent" v-model="form.hasProvidedConsentForm" :binary="true" />
+                    <div class="flex flex-row gap-2">
+                        <Checkbox id="consent" v-model="form.willingToSignContract" :binary="true" />
                         <label for="consent" class="font-bold block mb-2">Has Provided Consent Form?</label>
                     </div>
 
@@ -33,16 +20,15 @@
 
                         <OrderList v-if="form.preferences.length > 0" v-model="form.preferences" dragdrop data-key="id"
                             :list-style="{ 'height': '250px' }">
-
                             <template #option="{ option }">
-                                {{ option }}
+                                {{ allAvailablePreferences.find(p => p.id === option)?.name }}
                             </template>
                         </OrderList>
 
                         <div class="p-3 flex justify-between">
-                            <Button label="Add New" severity="secondary" text size="small" icon="pi pi-plus"
+                            <Button label="Add New" severity="secondary" text size="small" icon="i-mdi-plus"
                                 @click="showAddPreferenceDialog = true" />
-                            <Button label="Remove All" severity="danger" text size="small" icon="pi pi-times"
+                            <Button label="Remove All" severity="danger" text size="small" icon="i-mdi-times"
                                 @click="removeAll" />
                         </div>
 
@@ -58,7 +44,7 @@
                         </Card>
                     </div>
 
-                    <Button type="submit" variant="text" label="Next" class="p-mt-3" />
+                    <Button type="submit" variant="text" label="Next" />
                 </form>
             </template>
         </Card>
@@ -73,26 +59,28 @@ import Button from "primevue/button";
 import Card from "primevue/card";
 import Divider from "primevue/divider";
 import Listbox from "primevue/listbox";
-import type StudentPreferencesFormDto from "../models/StudentPreferencesFormDto";
-import type GroupAllocationOption from "../models/GroupAllocationOption";
 import { OrderList } from "primevue";
+import { useAuthStore } from "../store/auth";
+import type { StudentPreferencesDto } from "../dtos/student-preferences-dto";
+import type { ProjectDto } from "../dtos/project-dto";
+
+const authStore = useAuthStore();
 
 const form = ref({
-    firstName: "",
-    lastName: "",
-    email: "",
-    studentNumber: "",
-    hasProvidedConsentForm: false,
     preferences: [],
-} as StudentPreferencesFormDto);
+    willingToSignContract: false,
+    fileNames: [],
+    fileBlobs: [],
+    id: 0,
+} as StudentPreferencesDto);
 
 const allAvailablePreferences = ref([
-    { id: 1, name: "Preference 1" },
-    { id: 2, name: "Preference 2" },
-    { id: 3, name: "Preference 3" },
-    { id: 4, name: "Preference 4" },
-    { id: 5, name: "Preference 5" },
-] as GroupAllocationOption[]);
+    { id: 1, name: "Preference 1", description: "", requiresContract: false },
+    { id: 2, name: "Preference 2", description: "", requiresContract: true },
+    { id: 3, name: "Preference 3", description: "", requiresContract: false },
+    { id: 4, name: "Preference 4", description: "", requiresContract: true },
+    { id: 5, name: "Preference 5", description: "", requiresContract: false },
+] as ProjectDto[]);
 
 
 const showAddPreferenceDialog = ref(false);
@@ -105,15 +93,13 @@ const removeAll = () => {
     form.value.preferences = [];
 };
 
-const addPreference = (preference: GroupAllocationOption) => {
-    console.log("Adding preference", preference);
+const addPreference = (preference: ProjectDto[]) => {
     if (!form.value.preferences) {
         form.value.preferences = [];
     }
 
-    form.value.preferences.push(preference);
+    form.value.preferences.push(preference[0].id);
 
     showAddPreferenceDialog.value = false;
-    console.log("Preferences", form.value.preferences);
 };
 </script>
