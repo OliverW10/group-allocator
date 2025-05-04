22ba60211875c52319df8cf8252a0599261b0bac
OliverW10 <34265013+OliverW10@users.noreply.github.com>
2025-05-04 17:47:39 +1000

    dont allow duplicate manual allocations

--- frontend/src/components/AllocationsTable.vue
+++ frontend/src/components/AllocationsTable.vue
@@ -1,37 +1,66 @@
 <template>
+    <Button label="Clear auto-allocated" @click="clearAutoAllocated"></Button>
     <DataTable :value="allocations" :loading="false">
         <Column field="project" header="Project">
             <template #body="slotProps">
-                <Select v-model="slotProps.data.project" :options="props.projects" option-label="name" filter show-clear placeholder="Select Project" @change="maintainAllocationsList"></Select>
+                <Select v-model="slotProps.data.project" :options="remainingProjects" option-label="name" filter show-clear placeholder="Select Project" @change="onProjectChange"></Select>
             </template>
         </Column>
         <Column v-for="idx of [...Array(numStudents).keys()]" :key="idx" :field="students[idx]?.name ?? 'asdf'" :header="idx.toString()">
             <template #body="slotProps">
-                <Select v-if="slotProps.data.students" v-model="slotProps.data.students[idx]" :options="props.students" option-label="name" filter show-clear placeholder="Select Student" @change="maintainAllocationsList"></Select>
-                <!-- {{slotProps.data}} -->
+                <Select v-if="slotProps.data.students" v-model="slotProps.data.students[idx]" :options="remainingStudents" option-label="name" filter show-clear placeholder="Select Student" @change="maintainAllocationsList"></Select>
             </template>
         </Column>
     </DataTable>
 </template>
 <script setup lang="ts">
-import { computed, onMounted, watch } from 'vue';
+import { computed, onMounted, watch, type ComputedRef } from 'vue';
 import { ProjectDto } from '../dtos/project-dto';
 import { StudentInfoDto } from '../dtos/student-info-dto';
-import Select from 'primevue/select';
+import Select, { type SelectChangeEvent } from 'primevue/select';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
+import Button from 'primevue/button';
 
-interface PartialAllocation {
+export interface AllocatedStudentInfo extends StudentInfoDto {
+    manuallyAllocated: boolean,
+}
+
+export interface PartialAllocation {
     project: ProjectDto | null,
-    students: StudentInfoDto[],
+    manuallyAllocatedProject: boolean,
+    students: AllocatedStudentInfo[],
 }
 
 const props = defineProps<{
     students: StudentInfoDto[],
     projects: ProjectDto[],
 }>();
-
 const allocations = defineModel<PartialAllocation[]>();
+
+const studentsWithAllocated: ComputedRef<AllocatedStudentInfo[]> = computed(() => props.students?.map(s => {
+    // const casted = s as AllocatedStudentInfo;
+    // casted.manuallyAllocated = true
+    // return casted
+    return {
+        studentId: s.studentId,
+        userId: s.userId,
+        name: s.name,
+        email: s.email,
+        isVerified: s.isVerified,
+
+        manuallyAllocated: true,
+    }
+}) ?? [])
+const remainingStudents = computed(() => {
+    const usedStudents = allocations.value?.flatMap(aloc => aloc.students) ?? []
+    return studentsWithAllocated.value.filter(stu => !usedStudents.includes(stu))
+})
+const remainingProjects = computed(() => {
+    const usedProjects = allocations.value?.map(aloc => aloc.project).filter(x => x) ?? []
+    return props.projects.filter(proj => !usedProjects.includes(proj))
+})
+
 const numStudents = computed(() => {
     const lengths = allocations.value?.map(x => x.students.length) ?? [0]
     const maxLength = lengths.length != 0 ? Math.max(...lengths) : 0
@@ -40,7 +69,7 @@ const numStudents = computed(() => {
 })
 // TODO: fix page shift when selecting project
 
-const emptyAllocation: PartialAllocation = {project: null, students: []}
+const emptyAllocation: PartialAllocation = {project: null, manuallyAllocatedProject: false, students: []}
 const newEmptyAllocation = () => structuredClone(emptyAllocation)
 const isEmptyAllocaton = (allocation: PartialAllocation) => {
     return allocation.project == null && (allocation.students == null || allocation.students?.every(x => x == null))
@@ -61,6 +90,23 @@ const maintainAllocationsList = () => {
     if (allocations.value.length == 0 || !isEmptyAllocaton(allocations.value[l - 1])){
         allocations.value = [...allocations.value ?? [], newEmptyAllocation()]
     }
+}
+
+const clearAutoAllocated = () => {
+    for (const allocation of allocations.value ?? []) {
+        if (!allocation.manuallyAllocatedProject) {
+            allocation.project = null
+        }
+        allocation.students = allocation.students.filter(x => x.manuallyAllocated)
+    }
+    maintainAllocationsList()
+}
+
+const onProjectChange = (event: SelectChangeEvent) => {
+    for (const aloc of allocations.value?.filter(x => x.project == event.value) ?? []) {
+        aloc.manuallyAllocatedProject = true
+    }
+    maintainAllocationsList()
 }
 
 watch(allocations, maintainAllocationsList)
